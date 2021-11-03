aab22dd5ec9aa6bd11ae08fb2c93ea5b87797ce5
Curtis Chao <cchao38@my.bcit.ca>
2021-11-02 21:04:11 -0700

    added Login page and added firebase

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var  firebaseConfig = {
+    apiKey: "AIzaSyAA1tWXmyIol4sfaYogXHScTv7FEfAGiuw",
+    authDomain: "bby02-e6f54.firebaseapp.com",
+    projectId: "bby02-e6f54",
+    storageBucket: "bby02-e6f54.appspot.com",
+    messagingSenderId: "7644151226",
+    appId: "1:7644151226:web:40f0b93f45bb7309ecf183"  
+    };
+  
+  
+    const app = firebase.initializeApp(firebaseConfig);
+    
